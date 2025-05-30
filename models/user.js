403739fc9d1745f38fbb86131085bd771562ba81
Minamaher-sw <mosadefmena185@gmail.com>
2025-05-30 08:33:40 +0300

    add {timestamp:true} to add createdat and updateat for every user

--- models/user.js
+++ models/user.js
@@ -19,7 +19,7 @@ const userSchema = new Schema({
         },
         default: "user",
     },
-});
+},{ timestamps: true });
 
 const User = mongoose.model("User", userSchema);
 export default User;
