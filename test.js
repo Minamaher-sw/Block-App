d83a00a6c2a3ef948f413b3f6d3bccd383270060
Minamaher-sw <mosadefmena185@gmail.com>
2025-05-30 15:27:07 +0300

    adding nested validation and array achievment

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 
-import { error } from "console";
+import { error, profile } from "console";
 import * as yup from "yup"
 
 //fixed schema
@@ -50,4 +50,50 @@ user2schema.validate(user)
                             console.log(err.errors)
                         })
 
-                        
+// nested validation 
+const schema2 =yup.object({
+    profile:yup.object({
+        firstname:yup.string().required(),
+        lastname:yup.string().required(),
+    })
+})
+
+const task2={
+    profile:{
+        firstname :"mina",
+        lastname :"maher"
+    }
+}
+
+schema2.validate(task2).then((data)=>{
+    console.log(data)
+}).catch((err)=>{
+    console.log(err.errors)
+
+})
+
+// array achievment
+const arraySchema =yup.object({
+    tags:yup.array()
+            .of(yup.string()
+                    .strict()
+                    .min(2))
+                    .test(
+                            'not-numeric-only',
+                            (value) => {
+                                return isNaN(value); 
+                            }
+                        )
+            .min(1,"array must contain at least one element")
+});
+
+const arrayTask={
+    tags: [123] 
+}
+arraySchema.validate(arrayTask)
+                            .then((data)=>{
+                                console.log(data)
+                            })
+                            .catch(err=> console.log(err.errors[0]))
+
+// asunc validation
