8171b3f2d7232dcd30d007255d08ec2c242d8937
Minamaher-sw <mosadefmena185@gmail.com>
2025-05-30 12:24:52 +0300

    addign to two routes and import joi files and creation for joi middleware , using express.json middleware

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import express from "express";
 
-const app =express();
+import { userJOISchema ,updateUserSchema } from "./src/data/request_data.js";
+import request_validate from "./src/middleware/request_validate.js";
+
+const app =express(); 
+// parser
+app.use(express.json());
+
 const port=3001;
 const hostname="127.0.0.1"
 
 
+app.post("/users",request_validate(userJOISchema),(req,res,next)=>{
+    res.send("created");
+})
+app.patch("/users",request_validate(updateUserSchema),(req,res,next)=>{
+    res.send("created");
+})
 app.listen(port ,hostname ,(err)=>{
     if (err) console.log("Server not listen");
     console.log("server is listen on port ",port);
