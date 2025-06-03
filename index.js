c5bb5aa96fbca96f8082f59e16f6d3c6147428ab
Minamaher-sw <mosadefmena185@gmail.com>
2025-06-03 04:12:47 +0300

    modify index.js with bootstrap method

--- index.js
+++ index.js
@@ -1,23 +1,2 @@
-import express from "express";
-
-import { userJOISchema ,updateUserSchema } from "./src/data/request_data.js";
-import request_validate from "./src/middleware/request_validate.js";
-
-const app =express(); 
-// parser
-app.use(express.json());
-
-const port=3001;
-const hostname="127.0.0.1"
-
-
-app.post("/users",request_validate(userJOISchema),(req,res,next)=>{
-    res.send("created");
-})
-app.patch("/users",request_validate(updateUserSchema),(req,res,next)=>{
-    res.send("created");
-})
-app.listen(port ,hostname ,(err)=>{
-    if (err) console.log("Server not listen");
-    console.log("server is listen on port ",port);
-})
+import { bootstrap } from "./src/app.controller.js";
+bootstrap();
