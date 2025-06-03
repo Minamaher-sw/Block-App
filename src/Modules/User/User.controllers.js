1ade91bbe2d7b291b890ee0d7ea666267d183e26
Minamaher-sw <mosadefmena185@gmail.com>
2025-06-03 04:03:33 +0300

    user routes is finished

---
+++ src/Modules/User/User.controllers.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { signupValidate, updateUserValidate } from "./validate/User.validate.js";
+import { getAllUser, signup ,userLogin ,getUserById ,updateUser ,deleteUser} from "./Services/User,services.js";
+const userRouter =express.Router();
+
+userRouter.post("/",signupValidate,signup)
+userRouter.post("/login",userLogin)
+userRouter.get("/",getAllUser)
+userRouter.get("/:id",getUserById)
+userRouter.patch("/:id",updateUserValidate,updateUser)
+userRouter.delete("/:id",deleteUser);
+
+export default userRouter;
