7d892116a8469222197019b62cfc436158cbad11
Minamaher-sw <mosadefmena185@gmail.com>
2025-06-04 09:17:34 +0300

    auth midleware is finished

--- src/Modules/User/User.controllers.js
+++ src/Modules/User/User.controllers.js
@@ -3,6 +3,7 @@ import { signupValidate, updateUserValidate } from "./validate/User.validate.js"
 import { getAllUser, signup ,userLogin ,getUserById ,updateUser ,deleteUser} from "./Services/User.services.js";
 const userRouter =express.Router();
 
+
 userRouter.post("/",signupValidate,signup)
 userRouter.post("/login",userLogin)
 userRouter.get("/",getAllUser)
