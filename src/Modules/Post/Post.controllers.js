4805f6d6df2f273576662fc6ac9a047d72b17da1
Minamaher-sw <mosadefmena185@gmail.com>
2025-06-05 12:33:59 +0300

    updateing routes with adding authorize middleware and calling service methods

--- src/Modules/Post/Post.controllers.js
+++ src/Modules/Post/Post.controllers.js
@@ -1,9 +1,12 @@
 import express from "express"
+import { createPost, getAllPosts ,getPost ,updatePost ,deletePost} from "./Services/post.service";
+import { authorize } from "../../middleware/Authorization/user.author";
 const postRouter =express.Router();
 
-postRouter.post("/")
-postRouter.get("/")
-postRouter.patch("/:id")
-postRouter.delete("/:id");
+postRouter.post("/",authorize(["admin"]),createPost)
+postRouter.get("/",authorize(["admin"]) ,getAllPosts)
+postRouter.get("/:id",authorize(["user"]),getPost)
+postRouter.patch("/:id",authorize(["user"]),updatePost)
+postRouter.delete("/:id",authorize(["user"]),deletePost);
 
 export default postRouter;
